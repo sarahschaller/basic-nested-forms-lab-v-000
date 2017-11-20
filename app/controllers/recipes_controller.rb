742554a7b35cc1d57f1ef84988f8ac4dc41c34cd
Sarah Schaller <sarcsch@aol.com>
2017-11-20 20:09:35 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,13 @@ class RecipesController < ApplicationController
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+  private 
+
+  def recipe_params 
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
